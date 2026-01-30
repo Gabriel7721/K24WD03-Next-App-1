bbf6db3e754d4ab3a09f5b0fa69c55a43dba64b4
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-30 13:41:37 +0700

    first commit

---
+++ app/components/ProductCart.tsx
@@ -0,0 +1,12 @@
+import AddToCart from "./AddToCart";
+
+const ProductCart = () => {
+  return (
+    <main>
+      <h1>Product Cart</h1>
+      <AddToCart />
+    </main>
+  );
+};
+
+export default ProductCart;
