359b0eb9200fc13ed42fd2ff8515ae141e68bf25
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-30 13:53:47 +0700

    update next.config, thêm chức năng gửi request mỗi 5 giây / 1 giây cho từng page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/users/static",
+        headers: [{ key: "refresh", value: "5" }],
+      },
+      {
+        source: "/users/dynamic",
+        headers: [{ key: "refresh", value: "1" }],
+      },
+      {
+        source: "/users/isr",
+        headers: [{ key: "refresh", value: "5" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
