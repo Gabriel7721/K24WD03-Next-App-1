c97d12e877da673657819a8159762ac8447e5143
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-02-02 15:01:23 +0700

    u

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -43,3 +43,28 @@ export async function DELETE(request: NextRequest, { params }: Props) {
 
   return new NextResponse(null, { status: 204 });
 }
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const { id } = await params;
+
+  if (id > 10) {
+    return NextResponse.json({ message: "Product Not Found" });
+  }
+  const body = await request.json();
+
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.issues, { status: 400 });
+
+  // const updateUser = new User ({
+  //   name: validData.name ||,
+  //   price: validData.price,
+  // })
+
+  const validData = validation.data;
+  return NextResponse.json({
+    id: 3,
+    name: validData.name,
+    price: validData.price,
+  });
+}
