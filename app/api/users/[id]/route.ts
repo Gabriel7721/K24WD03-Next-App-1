20c5ae5d73cfb22fc8458d44ff360325c3ac3ee6
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-02-02 14:25:27 +0700

    udpate delete

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -31,3 +31,18 @@ export async function PUT(request: NextRequest, { params }: Props) {
 
   return NextResponse.json({ id: 3, name: body.name });
 }
+
+export async function DELETE(request: NextRequest, { params }: Props) {
+  const { id } = await params;
+
+  if (id > 10) {
+    return NextResponse.json({ message: "User Not Found" });
+  }
+
+  //   return NextResponse.json(
+  //     { server: `User with id ${id} has been deleted` },
+  //     { status: 200 },
+  //   );
+
+  return new NextResponse(null, { status: 204 });
+}
